3a8b37c945a3ab91736544c6d7b4718f5c228cac
Student <Student@LabVM2047644.n2hw0t5klamujflsj4qzkr31ug.jnbx.internal.cloudapp.net>
2026-05-25 13:56:16 +0200

    1st commmit

---
+++ src/main/java/Message.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.apkpoe;
+        import java.util.Random;
+/**
+ *
+ * @author Ncedolwenkosi
+ */
+public class Message {
+    // Declarations 
+    private String messageID;
+    private int messageNumber;
+    private String recipientNumber;
+    private String messageContent;
+    private String messageHash;
+            
+    
+}
